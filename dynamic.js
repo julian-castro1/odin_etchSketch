c8d108910243773174050519566ea15665ae6111
Julian Castro <julian_castro1@icloud.com>
2023-03-15 11:33:00 -0500

    added button functionality

--- dynamic.js
+++ dynamic.js
@@ -1,18 +1,54 @@
 let clearBut, blackBut, whiteBut, redBut, greenBut, blueBut, gradientBut, gridSizeInput;
+let brushColor = "black";
+let prevBrush = null;
 
+// buttons
+document.addEventListener("click", function(){
+    if(event.target.id == "clear"){ drawGrid(); }
+    if(event.target.className != "col"){ return; }
+    let but = event.target;
+    prevBrush = document.getElementById(brushColor);
+    prevBrush.style.border = "0";
+
+    brushColor = but.id;
+    document.getElementById(but.id).style.border = "4px solid #868585";
+})
+document.addEventListener("mouseover", function(){
+    if(event.target.className != "gridSpot"){ return; }
+    event.target.style.backgroundColor = getCol(brushColor);
+})
+
+// grid
 document.addEventListener("DOMContentLoaded", function () {
     // create the grid
     drawGrid();
 });
-
+document.addEventListener('input', drawGrid);
 function clearGrid(){
+    let child = document.getElementById("grid");
+    let parent = child.parentElement;
+    parent.removeChild(child);
 
+    let newChild = document.createElement("div");
+    newChild.id = "grid";
+    newChild.backgroundColor = "#c9c9c9";
+
+    parent.appendChild(newChild)
+}
+function getCol(colIn){
+    switch(colIn){
+        case "green" : return "#00d231"
+        case "gradient" : return "hsl("+ Math.random()*255 + ",100%,50%)"
+        // hsl(any, >30, >50)
+        default : return colIn;
+    }
 }
 function drawGrid(){
+    clearGrid();
     let container = document.getElementById("grid");
     let contStyle = window.getComputedStyle(container);
     let side = contStyle.getPropertyValue("min-height");
-    let lines = 20//document.getElementById("in").value;
+    let lines = document.getElementById("in").value;
 
     // range from 1 - 100 for the grid lines
     if(lines < 1){ lines = 1; }
